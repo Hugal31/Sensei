c784f57a98ce86219512d88353868528ec865042
laloge_h <hugo.laloge@epitech.eu>
2015-08-03 16:27:13 +0200

    Ajout du mode

--- teacher.py
+++ teacher.py
@@ -5,7 +5,7 @@
 ## Login   <laloge_h@epitech.net>
 ##
 ## Started on  Mon Aug  3 13:13:03 2015 Hugo Laloge
-## Last update Mon Aug  3 16:08:05 2015 Hugo Laloge
+## Last update Mon Aug  3 16:32:08 2015 Hugo Laloge
 ##
 
 import argparse
@@ -16,8 +16,10 @@ import sys
 from parser import parser
 
 class Teacher:
-    def __init__(self, file_name):
-        self.dictionary = parser(file_name)
+    def __init__(self, files):
+        self.dictionary = []
+        for file in files:
+            self.dictionary += parser(file)
 
     # Demande une fois chaque kana et donne le score
     def exam(self):
@@ -25,8 +27,8 @@ class Teacher:
         rate = len(self.dictionary)
         while len(self.dictionary) != 0:
             index = randint(0, len(self.dictionary) - 1)
-            syllabe = self.dictionary[index]
-            if (self.ask(syllabe)):
+            equivalence = self.dictionary[index]
+            if (self.ask(equivalence)):
                 score += 1
             self.dictionary.pop(index)
         print ('Score : %d/%d' % (score, rate))
@@ -38,8 +40,8 @@ class Teacher:
         rate = len(self.dictionary)
         while len(self.dictionary) != 0:
             index = randint(0, len(self.dictionary) - 1)
-            syllabe = self.dictionary[index]
-            if (self.ask(syllabe)):
+            equivalence = self.dictionary[index]
+            if (self.ask(equivalence)):
                 score += 1
                 self.dictionary.pop(index)
         print ('Finish !')
@@ -56,12 +58,24 @@ class Teacher:
 def arg_parse():
     parser = argparse.ArgumentParser(description=
                                      """Programme pour apprendre les kana et les kanji""")
-    parser.add_argument('file',
+
+    parser.add_argument('--mode',
+                        default='exam',
+                        choices=['exam', 'teach'],
+                        type=str,
+                        help='Mode d\'enseignement')
+
+    parser.add_argument('files',
+                        nargs='+',
                         type=argparse.FileType('r'),
                         help='Le fichier dictionnaire')
+
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = arg_parse()
-    teacher = Teacher(args.file)
-    teacher.exam()
+    teacher = Teacher(args.files)
+    if (args.mode == 'exam'):
+        teacher.exam()
+    else:
+        teacher.teach()
