6cb85c2fc565f99c3914a790409ec317674c4a34
laloge_h <hugo.laloge@epitech.eu>
2015-08-05 15:19:47 +0200

    Application graphique basique

--- sensei.py
+++ sensei.py
@@ -5,7 +5,7 @@
 ## Login   <laloge_h@epitech.net>
 ##
 ## Started on  Mon Aug  3 13:13:03 2015 Hugo Laloge
-## Last update Wed Aug  5 14:24:20 2015 Hugo Laloge
+## Last update Wed Aug  5 15:22:28 2015 Hugo Laloge
 ##
 
 import argparse
@@ -14,6 +14,7 @@ from random import randint
 import re
 import sys
 from time import time
+import tkinter
 
 from parser import parser
 
@@ -80,6 +81,55 @@ class Sensei:
                 print ('->', '|'.join(equivalence[1]))
                 return equivalence[1]
 
+class SenseiGui(Sensei):
+    def __init__(self, files):
+        super().__init__(files)
+        self.window = tkinter.Tk()
+
+        self.label = tkinter.Label(self.window, width=4, height=1, text='toto', font='TakaoMincho 74')
+        self.label.pack()
+
+        self.message = tkinter.Label(self.window, width=40, height=1, font='sans-serif 15')
+        self.message.pack()
+
+        self.entry = tkinter.Entry(self.window, width=20, font='sans-serif 15')
+        self.entry.bind('<Return>', self.get_answer)
+        self.entry.pack()
+
+        self.button = tkinter.Button(self.window, command=self.get_answer, text='Enter')
+        self.button.pack()
+
+    def exam(self):
+        self.mode = 'exam'
+        self.score = 0
+        self.rate = len(self.dictionary)
+        self.start_time = time()
+        self.ask_random()
+        self.window.mainloop()
+
+    def ask_random(self):
+        if len(self.dictionary) == 0:
+            print_score(self.score, self.rate, self.start_time)
+            exit()
+        self.current_index = randint(0, len(self.dictionary) - 1)
+        self.ask(self.dictionary[self.current_index])
+
+    def ask(self, equivalence):
+        self.label.config(text=equivalence[1][0])
+
+    def get_answer(self, garbage):
+        answer = self.entry.get()
+        if answer in self.dictionary[self.current_index][0]:
+            self.score += 1
+            self.message.config(text='Correct !')
+        else:
+            self.message.config(text='Incorrect, it was \'%s\'' % '|'.join(self.dictionary[self.current_index][0]))
+        if self.mode == 'exam':
+            self.dictionary.pop(self.current_index)
+        self.entry.delete(0, len(self.entry.get()))
+        self.ask_random()
+
+
 def arg_parse():
     parser = argparse.ArgumentParser(description=
                                      """Programme pour apprendre les kana et les kanji""")
@@ -95,11 +145,20 @@ def arg_parse():
                         type=argparse.FileType('r'),
                         help='Le fichier dictionnaire')
 
+    parser.add_argument('-g,--gui',
+                        const=True,
+                        dest='gui',
+                        nargs='?',
+                        help='Mode graphique')
+
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = arg_parse()
-    sensei = Sensei(args.files)
+    if args.gui:
+        sensei = SenseiGui(args.files)
+    else:
+        sensei = Sensei(args.files)
     if args.mode == 'exam':
         sensei.exam()
     elif args.mode == 'teach':
